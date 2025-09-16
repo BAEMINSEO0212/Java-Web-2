99920a9b6f618bda82dbf0da023a6f974a63707e
bae <bms030212@naver.com>
2025-09-16 23:34:04 +0900

    2주차 과제 풀이, 응용

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -2,12 +2,51 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model; // 추가된 부분
 
-@Controller // 이 클래스가 컨트롤러 역할을 한다고 Spring에게 알려줍니다.
+//import java.util.List; 2주차 과제 응용2 부분
+
+@Controller
 public class DemoController {
 
-    @GetMapping("/") // 사용자가 웹 브라우저에 "/" (기본 주소)를 입력하면 아래의 메소드를 실행합니다.
+    // 2주차 설정 부분(index.html 연결)
+    @GetMapping("/")
     public String home() {
-        return "index"; // templates 폴더에 있는 index.html 파일을 찾아서 사용자에게 보여줍니다.
+        return "index";
     }
+
+    // 2주차 추가 부분(hello.html 연결)
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "반갑습니다.");
+        return "hello";
+    }
+
+    // 2주차 추가 부분(hello2.html 연결)
+    @GetMapping("/hello2")
+    public String hello2(Model model) {
+        model.addAttribute("name", "배민서님.");
+        model.addAttribute("hello", "반갑습니다.");
+        model.addAttribute("today", "오늘.");
+        model.addAttribute("weather", "날씨는.");
+        model.addAttribute("comment", "매우 좋습니다.");
+        return "hello2";
+    }
+    // 2주차 과제 응용2 부분(List 사용)
+    /*
+     * @GetMapping("/hello2")
+     * public String hello2(Model model) {
+     * // 여러 개의 문자열 데이터를 하나의 List로 만듭니다.
+     * List<String> messageList = List.of(
+     * "홍길동님.",
+     * "방갑습니다.",
+     * "오늘.",
+     * "날씨는.",
+     * "매우 좋습니다.");
+     * // List 자체를 "messages"라는 이름으로 Model에 담습니다.
+     * model.addAttribute("messages", messageList);
+     * return "hello2";
+     * }
+     */
+
 }
