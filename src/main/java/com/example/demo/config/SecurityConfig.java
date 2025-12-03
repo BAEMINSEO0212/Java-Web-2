c1749a1b7c6f7a6943d8a792f988d562ee17b905
bae <bms030212@naver.com>
2025-12-03 13:50:26 +0900

    12주차 수업 내용

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -8,20 +8,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration // 스프링 설정 클래스 지정, 등록된 Bean 생성 시점
-@EnableWebSecurity // 스프링 보안 활성화
-public class SecurityConfig { // 스프링에서 보안 관리 클래스
-    @Bean // 명시적 의존성 주입 : Autowired와 다름
-    // 5.7버전 이저 WebSecurityConfigurerAdapter 사용
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // 설정을 비워둠
-        return http.build(); // 필터 체인을 통해 보안설정(HttpSecurity)을 반환
+        // [수정] http 객체로부터 설정을 시작해야 합니다.
+        http
+                .csrf(csrf -> csrf.disable()); // http 뒤에 이어서 작성
+
+        return http.build();
     }
 
-    @Bean // 암호화 설정
+    @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // 비밀번호 암호화 저장
+        return new BCryptPasswordEncoder();
     }
 }
-
-// 이 설정을 통해 내장된 로그인 페이지가 활성화가 취소됩니다.
